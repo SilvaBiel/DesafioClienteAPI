8a541c71024093bdbeab8e9f45417296c90eca97
Gabriel Santos <bielusantos@ucl.br>
2023-06-06 18:50:38 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioCliente/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ClienteManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioCliente.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ DesafioCliente/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClienteManagement.Models
+{
+    public class Cliente
+    {
+
+        [Key]
+        public int ClienteId { get; set; }
+        public string ClienteName { get; set; } = string.Empty;
+        public string ClienteLastName { get; set; } = string.Empty;
+        public int ClientePhone { get; set; }
+        public string ClienteEmail { get; set; } = string.Empty;
+        public string ClienteCep { get; set; } = string.Empty;
+        public int ClienteCPF { get; set; }
+    }
+
+}
